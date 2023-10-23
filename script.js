aea843fa626a7d894147d2bbd57c5e0affa047ad
PRIYANSHDUBEY <87897527+PRIYANSHDUBEY@users.noreply.github.com>
2023-10-24 01:37:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-function getFormvalue() {
-    //Write your code here
+// script.js
+function getFormValues() {
+    // Get form elements by their IDs
+    var firstNameInput = document.getElementById("firstName");
+    var lastNameInput = document.getElementById("lastName");
 
+    // Retrieve the values from the input fields
+    var firstName = firstNameInput.value;
+    var lastName = lastNameInput.value;
+
+    // Check if both first and last name are provided
+    if (firstName && lastName) {
+        alert("First Name: " + firstName + "\nLast Name: " + lastName);
+    } else {
+        alert("Please provide both first and last names.");
+    }
+
+    // Prevent the form from submitting and refreshing the page
+    return false;
 }
+
+// Add a form submission event listener
+var form = document.getElementById("myForm");
+form.addEventListener("submit", getFormValues);
